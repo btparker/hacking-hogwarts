b1f4079356a04301239c2c5415d1620b3bed957c
Tyler Parker <btylerparker@gmail.com>
2017-07-13 21:18:59 -0400

    Extracted from the time website

---
+++ time-sorting-hat-quiz/extracted/question_key.js
@@ -0,0 +1,191 @@
+[
+  {
+    "order": "1",
+    "id": "hppi_ordinary_person",
+    "question": "I am an ordinary person who is no better than others.",
+    "trait": "Honesty-Humility",
+    "code": "HH",
+    "direction": "1",
+    "count": "3"
+  },
+  {
+    "order": "2",
+    "id": "hppi_treat_as_superior",
+    "question": "I wouldn't want people to treat me as though I were superior to them.",
+    "trait": "Honesty-Humility",
+    "code": "HH",
+    "direction": "1",
+    "count": "3"
+  },
+  {
+    "order": "3",
+    "id": "hppi_critical_quarrelsome",
+    "question": "I see myself as critical, quarrelsome.",
+    "trait": "Agreeableness",
+    "code": "AGR",
+    "direction": "-1",
+    "count": "2"
+  },
+  {
+    "order": "4",
+    "id": "hppi_sympathetic_warm",
+    "question": "I see myself as sympathetic, warm.",
+    "trait": "Agreeableness",
+    "code": "AGR",
+    "direction": "1",
+    "count": "2"
+  },
+  {
+    "order": "5",
+    "id": "hppi_dependable_disciplined",
+    "question": "I see myself as dependable, self-disciplined.",
+    "trait": "Conscientiousness",
+    "code": "CONS",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "6",
+    "id": "hppi_disorganized_careless",
+    "question": "I see myself as disorganized, careless.",
+    "trait": "Conscientiousness",
+    "code": "CONS",
+    "direction": "-1",
+    "count": "6"
+  },
+  {
+    "order": "7",
+    "id": "hppi_face_fears",
+    "question": "I tend to face my fears.",
+    "trait": "Courage",
+    "code": "COUR",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "8",
+    "id": "hppi_feels_terrified",
+    "question": "Even if I feel terrified, I will stay in the situation until I have done what I need to do..",
+    "trait": "Courage",
+    "code": "COUR",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "9",
+    "id": "hppi_dangerous_things",
+    "question": "I will do things even though they seem to be dangerous.",
+    "trait": "Courage",
+    "code": "COUR",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "10",
+    "id": "hppi_worried_anxious",
+    "question": "If I am worried or anxious about something, I will do or face it anyway.",
+    "trait": "Courage",
+    "code": "COUR",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "11",
+    "id": "hppi_face_scary",
+    "question": "If there is an important reason to face something that scares me, I will face it.",
+    "trait": "Courage",
+    "code": "COUR",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "12",
+    "id": "hppi_not_back_down",
+    "question": "Even if something scares me, I will not back down.",
+    "trait": "Courage",
+    "code": "COUR",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "13",
+    "id": "hppi_manipulate_others",
+    "question": "I tend to manipulate others to get my way.",
+    "trait": "Machiavellianism",
+    "code": "MACH",
+    "direction": "1",
+    "count": "4"
+  },
+  {
+    "order": "14",
+    "id": "hppi_deceit_lied",
+    "question": "I have used deceit or lied to get my way.",
+    "trait": "Machiavellianism",
+    "code": "MACH",
+    "direction": "1",
+    "count": "4"
+  },
+  {
+    "order": "15",
+    "id": "hppi_flattery",
+    "question": "I have used flattery to get my way.",
+    "trait": "Machiavellianism",
+    "code": "MACH",
+    "direction": "1",
+    "count": "4"
+  },
+  {
+    "order": "16",
+    "id": "hppi_exploit_others",
+    "question": "I tend to exploit others towards my own end.",
+    "trait": "Machiavellianism",
+    "code": "MACH",
+    "direction": "1",
+    "count": "4"
+  },
+  {
+    "order": "17",
+    "id": "hppi_more_respect",
+    "question": "I think that I am entitled to more respect than the average person is.",
+    "trait": "Honesty-Humility",
+    "code": "HH",
+    "direction": "-1",
+    "count": "3"
+  },
+  {
+    "order": "18",
+    "id": "hppi_ambitious_goals",
+    "question": "When working, I often set ambitious goals for myself.",
+    "trait": "Conscientiousness",
+    "code": "CONS",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "19",
+    "id": "hppi_pushes_hard",
+    "question": "I often push myself very hard when trying to achieve a goal.",
+    "trait": "Conscientiousness",
+    "code": "CONS",
+    "direction": "1",
+    "count": "6"
+  },
+  {
+    "order": "20",
+    "id": "hppi_minimum_work",
+    "question": "I do only the minimum amount of work needed to get by.",
+    "trait": "Conscientiousness",
+    "code": "CONS",
+    "direction": "-1",
+    "count": "6"
+  },
+  {
+    "order": "21",
+    "id": "hppi_checks_work",
+    "question": "I often check my work over repeatedly to find any mistakes.",
+    "trait": "Conscientiousness",
+    "code": "CONS",
+    "direction": "1",
+    "count": "6"
+  }
+]
